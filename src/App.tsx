67e9174d46f52f912666792ff563971a61454da2
Anton Chikunov <anton@sharp-dev.net>
2020-12-11 11:36:15 +0300

    Sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import './assets/scss/main.scss';
+
+import AuthContainer from './components/auth/AuthContainer';
+import Login from './components/auth/Login';
+import SignUp from './components/auth/SignUp';
 
 function App() {
+  const [isSignedIn , setSignedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className={"page " + (!isSignedIn ? 'page--auth' : '')} >
+        <Switch>
+          <Route exact path="/">
+            <AuthContainer>
+              <Login />
+            </AuthContainer>
+          </Route>
+          <Route path="/signup">
+            <AuthContainer>
+              <SignUp />
+            </AuthContainer>
+          </Route>          
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
